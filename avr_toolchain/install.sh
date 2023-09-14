7417e9cb33cfada367d7e4c3cc88067edd70f857
kabsGOMspace <kabs@gomspace.com>
2023-09-14 10:04:43 +0200

    Fix bugs in avr toolchain install

--- avr_toolchain/install.sh
+++ avr_toolchain/install.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
+
 function check_install_file {
-	install_file=$1
-	if [ ! -f "$install_file" ]; then
-		echo "Installation file $install_file not found"
-		exit 1
-	fi
-	#return 1
+    install_file=$1
+    if [ ! -f "$install_file" ]; then
+        echo "Installation file $install_file not found"
+        exit 1
+    fi
 }
 
 set -e
-dry_run="no"
-while test $# -gt 0; do
-	arg=$1
-	if [ "$arg" == "-d" ]; then
-		dry_run="yes"
-	fi
-	if [ "$arg" == "-h" ]; then
-		echo "Usage: $0 [-d] [-h]"
-		echo "       -d   : dry run, dont install, just test"
-		echo "       -h   : print this help text"
-		exit 2
-	fi
-	shift
-done
+
 package_name="AVR32 Tool Chain"
 arch=`uname -m`
 
@@ -32,13 +19,10 @@ check_install_file avr32-utilities-$arch.tar.gz
 check_install_file atmel-headers-6.1.3.1475.zip
 echo "$package_name installation files verified OK"
 
-if [ $dry_run == "yes" ]; then
-echo "Installation is possible"
-else
 echo "Installing $package_name Binaries for $arch"
 tar xfz avr32-gnu-toolchain-3.4.2_gs1.435-linux.any.$arch.tar.gz
 if [ ! -d /usr/local/avr32 ]; then
-sudo mkdir /usr/local/avr32
+    sudo mkdir /usr/local/avr32
 fi
 sudo cp -ra avr32-gnu-toolchain-linux_$arch/* /usr/local/avr32/
 rm -rf ./avr32-gnu-toolchain-linux_$arch
@@ -56,20 +40,32 @@ sudo cp -ra atmel-headers-6.1.3.1475/avr32/ /usr/local/avr32/avr32/include/
 rm -rf atmel-headers-6.1.3.1475
 
 echo "Checking environment"
+
 # Setup path
 add_to_path='/usr/local/avr32/bin'
 if ! grep -q "PATH.*$add_to_path" ~/.profile; then
-echo "Setting up PATH: Adding $add_to_path to PATH"
-echo "PATH=\$PATH:$add_to_path" >> ~/.profile
-export PATH=\$PATH:$add_to_path
-echo $PATH
-fi
-# Setup aliases
-if ! grep -q "alias waf" ~/.bash_aliases; then
-echo "Setting up aliases"
-echo "alias waf=./waf" >> ~/.bash_aliases
-echo "alias a=ack-grep" >> ~/.bash_aliases
-fi
-echo "$package_name installation completed OK"
+    echo "Setting up PATH: Adding $add_to_path to PATH"
+    echo "PATH=\$PATH:$add_to_path" >> ~/.profile
+    source ~/.profile
 fi
 
+# Create bash alias file if it does not exist
+if [ ! -e ~/.bash_aliases ]; then
+    touch ~/.bash_aliases
+fi
+
+# Setup aliases
+if ! grep -qF "alias waf" ~/.bash_aliases; then
+    echo "Setting up aliases"
+    echo "alias waf=./waf" >> ~/.bash_aliases
+    echo "alias a=ack-grep" >> ~/.bash_aliases
+fi
+
+# Ubuntu 18 and onwards is missing libmpfr.so.4, but a symbolic link can be created from libmpfr.so.6
+VERSION=$(lsb_release -sr)
+if [[ "$VERSION" =~ ^("18.04"|"20.04"|"22.04")$ ]]; then
+    echo "Ubuntu $VERSION detected. Creating symbolic link for libmpfr.so.4 -> libmpfr.so.6"
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libmpfr.so.6 /usr/lib/x86_64-linux-gnu/libmpfr.so.4
+fi
+
+echo "$package_name installation completed OK"
